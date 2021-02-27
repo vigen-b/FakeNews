436786d71b4a391fe41f108c683cbf52f52a246f
Vigen Baghdasaryan <vigbagh@gmail.com>
2021-02-27 21:59:41 +0400

    Define models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        "auth.User", related_name="news", on_delete=models.CASCADE, null=False
+    )
+    text = models.TextField(blank=False, null=False)
+    main_category = models.OneToOneField(
+        "Category", on_delete=models.CASCADE, related_name="main_news", null=False
+    )
+
+    class Meta:
+        ordering = ["created_at"]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False, unique=True)
+    news = models.ManyToManyField(News, blank=True)
