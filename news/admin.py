f6fcec3cfdda5b8e8c35db963c223f67eca68206
Vigen Baghdasaryan <vigbagh@gmail.com>
2021-02-28 16:20:37 +0400

    Update news page in admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import News, Category
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "title",
+        "main_category_link",
+        "created_at",
+    )
 
+    def main_category_link(self, obj):
+        url = (
+            reverse("admin:news_category_changelist") + f"{obj.main_category.id}/change"
+        )
+        return format_html('<a href="{}">{}</a>', url, obj.main_category.name)
 
-# admin.site.register(News)
+    main_category_link.short_description = "Main Category"
 
 
 admin.site.register(Category)
